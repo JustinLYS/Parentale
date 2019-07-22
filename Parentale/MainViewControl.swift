b186401724985c659ea930d7fd0c5ab813b5d7f6
Justin Lim <ysjustinlim@gmail.com>
2019-07-22 18:53:52 +0800

    Parentale ScrollBar is running actively

--- Parentale/MainViewControl.swift
+++ Parentale/MainViewControl.swift
@@ -16,12 +16,23 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICo
     var formattedDate = DateFormatter()
     var isDetailSegueAdd = false
 
+    @IBOutlet weak var miniTitle: UILabel!
+    @IBOutlet weak var headerHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var titleTopConstraint: NSLayoutConstraint!
     
-    @IBOutlet weak var deedCollectionView: UICollectionView! {
-        didSet {
-            
-        }
-    }
+    @IBOutlet weak var expTitle: UILabel!
+    @IBOutlet weak var expSearch: UIButton!
+    @IBOutlet weak var expAddLabel: UILabel!
+    @IBOutlet weak var expAddBtn: UIButton!
+    @IBOutlet weak var expImage: UIImageView!
+    
+    let maxHeaderHeight: CGFloat = 285
+    let minHeaderHeight: CGFloat = 50
+    
+    var previousScrollOffset: CGFloat = 0;
+    
+    
+    @IBOutlet weak var deedCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,9 +51,126 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICo
                 
         formattedDate.dateFormat = "d MMM"
         
-        let testDeed = Deed(date: Date(), desc: "TESTING a very very long long question that might break code", question: "CAN I WORK?")
-        deedsCompiler.addDeed(newDeed: testDeed)
+        let testDeed1 = Deed(date: Date(), desc: "TESTING a very very long long question that might break code", question: "CAN I WORK?")
+        deedsCompiler.addDeed(newDeed: testDeed1)
         
+        let testDeed2 = Deed(date: Date(), desc: "TESTING a very very long long question that might break code", question: "CAN I WORK?")
+        deedsCompiler.addDeed(newDeed: testDeed2)
+        let testDeed3 = Deed(date: Date(), desc: "TESTING a very very long long question that might break code", question: "CAN I WORK?")
+        deedsCompiler.addDeed(newDeed: testDeed3)
+        let testDeed4 = Deed(date: Date(), desc: "TESTING a very very long long question that might break code", question: "CAN I WORK?")
+        deedsCompiler.addDeed(newDeed: testDeed4)
+        let testDeed5 = Deed(date: Date(), desc: "TESTING a very very long long question that might break code", question: "CAN I WORK?")
+        deedsCompiler.addDeed(newDeed: testDeed5)
+        let testDeed6 = Deed(date: Date(), desc: "TESTING a very very long long question that might break code", question: "CAN I WORK?")
+        deedsCompiler.addDeed(newDeed: testDeed6)
+        let testDeed7 = Deed(date: Date(), desc: "TESTING a very very long long question that might break code", question: "CAN I WORK?")
+        deedsCompiler.addDeed(newDeed: testDeed7)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.headerHeightConstraint.constant = self.maxHeaderHeight
+        updateHeader()
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let scrollDiff = scrollView.contentOffset.y - self.previousScrollOffset
+        
+        let absoluteTop: CGFloat = 0;
+        let absoluteBottom: CGFloat = scrollView.contentSize.height - scrollView.frame.size.height;
+        
+        let isScrollingDown = scrollDiff > 0 && scrollView.contentOffset.y > absoluteTop
+        let isScrollingUp = scrollDiff < 0 && scrollView.contentOffset.y < absoluteBottom
+        
+        if canAnimateHeader(scrollView) {
+            
+            // Calculate new header height
+            var newHeight = self.headerHeightConstraint.constant
+            if isScrollingDown {
+                newHeight = max(self.minHeaderHeight, self.headerHeightConstraint.constant - abs(scrollDiff))
+            } else if isScrollingUp {
+                if (deedCollectionView.contentOffset.y < 0) {
+                    newHeight = min(self.maxHeaderHeight, self.headerHeightConstraint.constant + abs(scrollDiff))
+                }
+                
+            }
+            
+            // Header needs to animate
+            if newHeight != self.headerHeightConstraint.constant {
+                self.headerHeightConstraint.constant = newHeight
+                self.updateHeader()
+                self.setScrollPosition(self.previousScrollOffset)
+            }
+            
+            self.previousScrollOffset = scrollView.contentOffset.y
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        self.scrollViewDidStopScrolling()
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if !decelerate {
+            self.scrollViewDidStopScrolling()
+        }
+    }
+    
+    func scrollViewDidStopScrolling() {
+        let range = self.maxHeaderHeight - self.minHeaderHeight
+        let midPoint = self.minHeaderHeight + (range / 2)
+        
+        if self.headerHeightConstraint.constant > midPoint {
+            self.expandHeader()
+        } else {
+            self.collapseHeader()
+        }
+    }
+    
+    func canAnimateHeader(_ scrollView: UIScrollView) -> Bool {
+        // Calculate the size of the scrollView when header is collapsed
+        let scrollViewMaxHeight = scrollView.frame.height + self.headerHeightConstraint.constant - minHeaderHeight
+        
+        // Make sure that when header is collapsed, there is still room to scroll
+        return scrollView.contentSize.height > scrollViewMaxHeight
+    }
+    
+    func collapseHeader() {
+        self.view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.2, animations: {
+            self.headerHeightConstraint.constant = self.minHeaderHeight
+            self.updateHeader()
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    func expandHeader() {
+        self.view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.2, animations: {
+            self.headerHeightConstraint.constant = self.maxHeaderHeight
+            self.updateHeader()
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    func setScrollPosition(_ position: CGFloat) {
+        self.deedCollectionView.contentOffset = CGPoint(x: self.deedCollectionView.contentOffset.x, y: position)
+    }
+    
+    func updateHeader() {
+        let range = self.maxHeaderHeight - self.minHeaderHeight
+        let openAmount = self.headerHeightConstraint.constant - self.minHeaderHeight
+        let percentage = openAmount / range
+        
+        self.titleTopConstraint.constant = -openAmount + 10
+        // Makes it dissapear
+        self.expTitle.alpha = percentage
+        self.expSearch.alpha = percentage
+        self.expImage.alpha = percentage
+        self.expAddLabel.alpha = percentage
+        self.expAddBtn.alpha = percentage
     }
     
     
